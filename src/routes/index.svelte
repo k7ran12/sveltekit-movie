42ef79088f0c79b06d0e8e6e69e9cb5cc166da4e
Cristian Antonio Valverde <tauro24_vs@hotmail.com>
2022-07-11 20:33:57 -0500

    Final

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,7 @@
 <script context="module">
     export async function load({fetch}){
         const res = await fetch(
-            `https://api.themoviedb.org/3/movie/popular?api_key=8b602b9bd07ee12f35f3ffa0beb7df98&language=en-US&page=1`
+            `https://api.themoviedb.org/3/movie/popular?api_key=${import.meta.env.VITE_API}&language=en-US&page=1`
             );
             const data = await res.json();            
             if(res.ok){
@@ -11,7 +11,7 @@
             }
     }
 </script>
-<script>    
+<script>        
     import PopularMovie from "../components/PopularMovie.svelte";
     import SearchMovies from "../components/SearchMovies.svelte";
     export let popular; 
--- src/routes/search/[id].svelte
+++ src/routes/search/[id].svelte
@@ -1,7 +1,7 @@
 <script context="module">
     export async function load({fetch,params}){
         const res = await fetch(                        
-            `https://api.themoviedb.org/3/search/movie?api_key=8b602b9bd07ee12f35f3ffa0beb7df98&language=en-US&query=${params.id}&include_adult=false`
+            `https://api.themoviedb.org/3/search/movie?api_key=${import.meta.env.VITE_API}&language=en-US&query=${params.id}&include_adult=false`
             );
             const data = await res.json();
             console.log(data);
